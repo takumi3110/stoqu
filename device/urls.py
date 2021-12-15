bfacca372c5f5474afdb96c226cabb111da9a2e3
takumi3110 <takumi929s@prestigein.com>
2021-12-15 14:47:00 +0900

    [add]router & appname

--- device/urls.py
+++ device/urls.py
@@ -1,1 +1,15 @@
 # -*- cording:utf-8 -*-
+
+from django.urls import path
+from rest_framework import routers
+
+from . import views
+
+app_name = 'device'
+
+router = routers.DefaultRouter()
+router.register(r'cpu', views.CPUViewSet)
+router.register(r'storage', views.StorageViewSet)
+router.register(r'pc_spec', views.PCSpecViewSet)
+router.register(r'pc', views.PCViewSet)
+router.register(r'item', views.ItemViewSet)
