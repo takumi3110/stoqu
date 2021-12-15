14bede4d94cb77fcf7c796fa1d68612d3097ce89
takumi3110 <takumi929s@prestigein.com>
2021-12-15 12:56:15 +0900

    [fix]SECRET_KEYを除外

--- stoqu/settings/base.py
+++ stoqu/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mx%znj!$o!*c1%zi+#$u&$d*c!@s4vdw+b6ktfcn529qc&9219'
+SECRET_KEY = keys
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
