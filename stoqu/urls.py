cde861f7e42669621377e9781bc64d0a358eecb1
takumi3110 <takumi929s@prestigein.com>
2021-12-15 14:54:08 +0900

    [add]static, staticfiles_urlpatterns

--- stoqu/urls.py
+++ stoqu/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
+from .settings.base import MEDIA_URL, MEDIA_ROOT
 from device.urls import router as device_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/device/', include(device_router.urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(MEDIA_URL, documment_root=MEDIA_ROOT)
