88b68bba8df55ae9047c128f1ea9b9afdc4230b8
takumi3110 <takumi929s@prestigein.com>
2021-12-15 14:42:29 +0900

    [add]viewset

--- device/views.py
+++ device/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import CPU, Storage, PCSpec, PC, Item
+from .serializer import CPUSerializer, StorageSerializer, PCSpecSerializer, PCSerializer, ItemSerializer
+
+
+class CPUViewSet(viewsets.ModelViewSet):
+	queryset = CPU.objects.all()
+	serializer_class = CPUSerializer
+
+
+class StorageViewSet(viewsets.ModelViewSet):
+	queryset = Storage.objects.all()
+	serializer_class = StorageSerializer
+
+
+class PCSpecViewSet(viewsets.ModelViewSet):
+	queryset = PCSpec.objects.all()
+	serializer_class = PCSpecSerializer
+
+
+class PCViewSet(viewsets.ModelViewSet):
+	queryset = PC.objects.all()
+	serializer_class = PCSerializer
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+	queryset = Item.objects.all()
+	serializer_class = ItemSerializer
