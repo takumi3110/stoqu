747c86a2acc3f39f663e394cd904583fa3b6fc22
takumi3110 <takumi929s@prestigein.com>
2021-12-15 16:32:22 +0900

    [add]stock_router

--- stoqu/urls.py
+++ stoqu/urls.py
@@ -4,10 +4,12 @@ from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 from .settings.base import MEDIA_URL, MEDIA_ROOT
 from device.urls import router as device_router
+from stock.urls import router as stock_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/device/', include(device_router.urls)),
+    path('api/v1/stock/', include(stock_router.urls)),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
