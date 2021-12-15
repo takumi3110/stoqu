748cf0ba632903972c365a7dd36f0479f82b2f72
takumi3110 <takumi929s@prestigein.com>
2021-12-15 18:40:35 +0900

    [add]listview detailview

--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
+
 from rest_framework import viewsets
 
 from .models import Option, Base, Storage
@@ -18,3 +21,20 @@ class BaseViewSet(viewsets.ModelViewSet):
 class StorageViewSet(viewsets.ModelViewSet):
 	queryset = Storage.objects.all()
 	serializer_class = StorageSerializer
+
+
+class StorageListView(LoginRequiredMixin, ListView):
+	model = Storage
+	template_name = 'stock/storage_list.html'
+	paginate_by = 20
+
+	def get_context_data(self, *, object_list=None, **kwargs):
+		context = super().get_context_data(**kwargs)
+		base = Base.objects.all()
+		context['base_list'] = base
+		return context
+
+
+class StorageDetailView(LoginRequiredMixin, DetailView):
+	model = Storage
+	template_name = 'stock/storage_detail.html'
