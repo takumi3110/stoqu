e6dd7f7d27a4d5206132d008ebbe6c566a958649
takumi3110 <takumi929s@prestigein.com>
2021-12-15 16:25:05 +0900

    [add]router作成

--- stock/urls.py
+++ stock/urls.py
@@ -1,1 +1,13 @@
 # -*- cording:utf-8 -*-
+
+from django.urls import path
+from rest_framework import routers
+
+from . import views
+
+app_name = 'stock'
+
+router = routers.DefaultRouter()
+router.register(r'option', views.OptionViewSet)
+router.register(r'base', views.BaseViewSet)
+router.register(r'storage', views.StorageSerializer)
