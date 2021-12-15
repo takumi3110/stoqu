e3d3b2896e08a67cfce688dae334d946e198cec9
takumi3110 <takumi929s@prestigein.com>
2021-12-15 16:23:46 +0900

    [add]各テーブルのviewset作成

--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Option, Base, Storage
+from .serializer import OptionSerializer, BaseSerializer, StorageSerializer
+
+
+class OptionViewSet(viewsets.ModelViewSet):
+	queryset = Option.objects.all()
+	serializer_class = OptionSerializer
+
+
+class BaseViewSet(viewsets.ModelViewSet):
+	queryset = Base.objects.all()
+	serializer_class = BaseSerializer
+
+
+class StorageViewSet(viewsets.ModelViewSet):
+	queryset = Storage.objects.all()
+	serializer_class = StorageSerializer
