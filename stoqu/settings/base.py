148992b098b45c3ffd8b731c9cb6b40d6642d8d0
takumi3110 <takumi929s@prestigein.com>
2021-12-22 15:44:35 +0900

    [fix]staticの調整

--- stoqu/settings/base.py
+++ stoqu/settings/base.py
@@ -18,7 +18,7 @@ from .local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_DIR = BASE_DIR.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -123,14 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [os.path.join(STATIC_DIR, 'static')]
 # デプロイ用
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(STATIC_DIR, 'media')
 
 REST_FRAMEWORK = {
 	'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
