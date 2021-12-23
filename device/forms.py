f7794befb22a558a55455cb3ec1efabaafbb4b4b
19020081 <takumi929s@prestigein.com>
2021-12-23 17:27:20 +0900

    [add]各モデルのmodalform

--- device/forms.py
+++ device/forms.py
@@ -1,1 +1,39 @@
 # -*- coding:utf-8 -*-
+
+from django import forms
+from bootstrap_modal_forms.forms import BSModalModelForm
+
+from .models import PC, PCSpec, Item
+
+
+class PCCreateBSModalForm(BSModalModelForm):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		for field in self.fields.values():
+			field.widget.attrs['class'] = 'form-control'
+
+	class Meta:
+		model = PC
+		fields = ('category', 'maker', 'name', 'model_number', 'img')
+
+
+class ItemBSModalForm(BSModalModelForm):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		for field in self.fields.values():
+			field.widget.attrs['class'] = 'form-control'
+
+	class Meta:
+		model = Item
+		fields = ('pc', 'spec')
+
+
+class PCSpecCreateBSModalForm(BSModalModelForm):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		for field in self.fields.values():
+			field.widget.attrs['class'] = 'form-control'
+
+	class Meta:
+		model = PCSpec
+		fields = ('category', 'cpu', 'memory', 'storage', 'size', 'camera', 'fingerprint', 'numpad', 'lan', 'usb', 'hdmi', 'vga')
