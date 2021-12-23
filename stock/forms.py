4d32b8ac96fe8b1b113a92abcf7c6bfe1dc97f19
19020081 <takumi929s@prestigein.com>
2021-12-23 13:00:06 +0900

    [add]bsmodalforms

--- stock/forms.py
+++ stock/forms.py
@@ -1,1 +1,28 @@
 # -*- coding:utf-8 -*-
+
+from django import forms
+from bootstrap_modal_forms.forms import BSModalModelForm
+
+from .models import Storage
+
+
+class StorageCreateBSModalForm(BSModalModelForm):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		for field in self.fields.values():
+			field.widget.attrs['class'] = 'form-control'
+
+	class Meta:
+		model = Storage
+		fields = ('order_number', 'item', 'price', 'quantity', 'option', 'base', 'delivery_date', 'remarks')
+
+
+class StorageUpdateBSModalForm(BSModalModelForm):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		for field in self.fields.values():
+			field.widget.attrs['class'] = 'form-control'
+
+	class Meta:
+		model = Storage
+		fields = ('order_number', 'item', 'price', 'quantity', 'option', 'base', 'delivery_date', 'remarks')
