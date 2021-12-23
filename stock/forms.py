9564acd8c99e1a0cb1ab3e9a431053460fc05dbe
19020081 <takumi929s@prestigein.com>
2021-12-23 14:29:19 +0900

    [add]option登録用modalform

--- stock/forms.py
+++ stock/forms.py
@@ -3,7 +3,7 @@
 from django import forms
 from bootstrap_modal_forms.forms import BSModalModelForm
 
-from .models import Storage
+from .models import Storage, Option
 
 
 class StorageCreateBSModalForm(BSModalModelForm):
@@ -26,3 +26,14 @@ class StorageUpdateBSModalForm(BSModalModelForm):
 	class Meta:
 		model = Storage
 		fields = ('order_number', 'item', 'price', 'quantity', 'option', 'base', 'delivery_date', 'remarks')
+
+
+class OptionCreateBSModalForm(BSModalModelForm):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		for field in self.fields.values():
+			field.widget.attrs['class'] = 'form-control'
+
+	class Meta:
+		model = Option
+		fields = ('maker', 'name', 'price', 'quantity', 'remarks')
