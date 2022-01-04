e750c5df0a0f0f7ebe684ce3f150726eec4c18d7
takumi3110 <takumi929s@prestigein.com>
2022-01-04 18:23:17 +0900

    add:cart

--- stock/urls.py
+++ stock/urls.py
@@ -22,5 +22,9 @@ urlpatterns = [
 	path('get_data/', views.create_storage_data, name='get_data'),
 	path('create/', views.StorageItemCreateView.as_view(), name='storage_create'),
 	path('update/<int:pk>', views.StorageItemUpdateView.as_view(), name='storage_update'),
-	path('optionCreate/', views.OptionCreateView.as_view(), name='option_create')
+	path('optionCreate/', views.OptionCreateView.as_view(), name='option_create'),
+	path('cart_list/', views.StorageCartListView.as_view(), name='cart'),
+	path('add_item/<int:pk>', views.add_item, name='add_item'),
+	path('reduce_item/<int:pk>', views.reduce_cart, name='reduce_cart'),
+	path('remove_item/<int:pk>', views.remove_cart, name='remove_cart'),
 ]
