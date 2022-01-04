6a8c12768f0b500c674999d3f331727aea2b236b
19020081 <takumi929s@prestigein.com>
2022-01-04 12:49:38 +0900

    add:order serializer class

--- stock/serializer.py
+++ stock/serializer.py
@@ -2,7 +2,7 @@
 
 from rest_framework import serializers
 
-from .models import Option, Base, StorageItem
+from .models import *
 from device.serializer import PCSerializer
 
 
@@ -26,3 +26,27 @@ class StorageItemSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = StorageItem
 		fields = ('order_number', 'item', 'price', 'quantity', 'option', 'base', 'delivery_date', 'remarks')
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = OrderItem
+		fields = ('storage_item', 'quantity', 'ordered')
+
+
+class StorageCartSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = StorageCart
+		fields = ('requester', 'order_item', 'order_item')
+
+
+class ApproveSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Approve
+		fields = ('last_name', 'first_name', 'dept_code', 'dept_name')
+
+
+class OrderInfoSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = OrderInfo
+		fields = ('number', 'ticket', 'storage_cart', 'approve', 'requester', 'contact_user', 'ordered_at', 'ordered')
