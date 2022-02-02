88d1cf5dd03e6689821ab559f67849f1e2bdb257
takumi3110 <takumi929s@prestigein.com>
2022-02-02 18:47:55 +0900

    add:ajax function

--- static/js/stock.js
+++ static/js/stock.js
@@ -7,3 +7,43 @@ document.addEventListener('DOMContentLoaded', function() {
         return `${acc}<span class="char">${curr}</span>`;
     }, "");
 });
+
+function radioCheck(id, postData, url) {
+    let checkOption = document.querySelector(id);
+        checkOption.addEventListener('click', function() {
+            console.log(document.querySelector(id).value);
+            ajaxPost(postData, url);
+        });
+}
+
+function ajaxPost(postData, url) {
+    kittingPrice.textContent = "";
+    // const init = {method: 'GET'};
+    const init = {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+    };
+
+    fetch(url, init)
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw Error();
+            }
+        })
+        .then(data => {
+            let price = 0;
+            for(const orderItem of data.results) {
+                price += orderItem.kitting_plan.price;
+                kittingPrice.innerHTML = price;
+            }
+        })
+        .catch (error => {
+            console.log(error);
+        });
+}
