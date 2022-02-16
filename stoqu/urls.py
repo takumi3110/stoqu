a22baf02a87b1c70d8be1c799471d141a0400ffb
takumi3110 <takumi929s@prestigein.com>
2022-02-16 18:30:49 +0900

    add:user urls.py

--- stoqu/urls.py
+++ stoqu/urls.py
@@ -5,13 +5,16 @@ from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 from .settings.base import MEDIA_URL, MEDIA_ROOT
 from device.urls import router as device_router
 from stock.urls import router as stock_router
+from user.urls import router as user_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/device/', include(device_router.urls)),
     path('api/v1/stock/', include(stock_router.urls)),
+    path('api/v1/user/', includ(user_router.urls)),
     path('', include('stock.urls')),
-    path('device/', include('device.urls'))
+    path('device/', include('device.urls')),
+    path('user/', include('user.urls'))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
