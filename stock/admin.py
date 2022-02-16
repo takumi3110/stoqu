9b948627495aba4c6d3dd1cabe5d0052fe3e9f31
takumi3110 <takumi929s@prestigein.com>
2022-02-16 16:21:55 +0900

    fix:search fields 変更

--- stock/admin.py
+++ stock/admin.py
@@ -17,9 +17,10 @@ class StorageItemAdmin(admin.ModelAdmin):
 	list_display = ('base', 'order_number', 'item', 'price', 'quantity', 'registration_at')
 	list_display_links = ('base', 'order_number', 'item', 'price', 'quantity', 'registration_at')
 	list_filter = ('order_number', 'base')
-	search_fields = ('order_number', 'item', 'base')
+	search_fields = ('order_number', 'item__pc__name', 'base__name')
 	actions_on_bottom = True
 	filter_horizontal = ['option']
+	ordering = ['base']
 
 
 @admin.register(OrderItem)
@@ -27,16 +28,16 @@ class OrderItemAdmin(admin.ModelAdmin):
 	list_display = ('storage_item', 'quantity', 'ordered', 'kitting_plan', 'requester')
 	list_display_links = ('storage_item', 'quantity', 'kitting_plan', 'requester')
 	list_filter = ('ordered', 'kitting_plan')
-	search_fields = ('storage_item',)
+	search_fields = ['storage_item__item__pc__name']
 	actions_on_bottom = True
 
 
 @admin.register(StorageCart)
 class StorageCartAdmin(admin.ModelAdmin):
-	list_display = ('requester', 'ordered')
-	list_display_links = ('requester',)
+	list_display = ('requester', 'price', 'ordered')
+	list_display_links = ('requester', 'price', 'ordered')
 	list_filter = ('requester', 'ordered')
-	search_fields = ('requester',)
+	search_fields = ('requester__user__screenname',)
 	actions_on_bottom = True
 	filter_horizontal = ['order_item']
 
@@ -55,7 +56,7 @@ class OrderInfoAdmin(admin.ModelAdmin):
 	list_display = ('number', 'ticket', 'approve', 'requester', 'contact_user', 'ordered_at')
 	list_display_links = ('number', 'ticket', 'approve', 'requester', 'contact_user', 'ordered_at')
 	list_filter = ('approve', 'requester', 'contact_user')
-	search_fields = ('number', 'ticket', 'approve', 'requester')
+	search_fields = ('number', 'ticket', 'approve')
 	actions_on_bottom = True
 
 
