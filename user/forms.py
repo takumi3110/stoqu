bf1df5665aae62f6d99111dac06d05d3fdb5e236
takumi3110 <takumi929s@prestigein.com>
2022-02-16 18:45:08 +0900

    fix:modalform からmodalmodelformに変更

--- user/forms.py
+++ user/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from bootstrap_modal_forms.forms import BSModalForm
+from bootstrap_modal_forms.forms import BSModalModelForm
 
 from .models import Requester
 
 
-class RequesterUpdateBsModalForm(BSModalForm):
+class RequesterUpdateBsModalForm(BSModalModelForm):
 	def __init__(self, *args, **kwargs):
 		super(RequesterUpdateBsModalForm, self).__init__(*args, **kwargs)
 		for field in self.fields.values():
