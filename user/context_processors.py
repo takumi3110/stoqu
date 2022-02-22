2127df93f32e8ee14b343b7a01cea8cbdc9d79be
takumi3110 <takumi929s@prestigein.com>
2022-02-22 17:53:43 +0900

    fix:保留

--- user/context_processors.py
+++ user/context_processors.py
@@ -1,6 +1,13 @@
-from .models import Requester
+from django.contrib.auth.decorators import login_required
+
+from .models import User, Requester
 
 
+@login_required()
 def context(request):
-	requester = Requester.objects.get(user=request.user)
-	return {'requester': requester}
+	user = User.objects.all()
+	if request.user in user:
+		requester = Requester.objects.get(user=request.user)
+		return {'requester': requester}
+	else:
+		return None
