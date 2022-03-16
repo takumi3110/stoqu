41f8ad7162e85982a4bd8ffe63f6a33e4a813d35
takumi3110 <takumi929s@prestigein.com>
2022-03-16 15:40:59 +0900

    add:specの表示

--- static/js/quote/quote.js
+++ static/js/quote/quote.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
 class category {
 	constructor() {
+		this.selectedCategory = document.querySelector('#pcCategory');
+		this.cpu = document.querySelector('#detailCpu');
+		this.memory = document.querySelector('#detailMemory');
+		this.storage = document.querySelector('#detailStorage');
 		this._init();
 	}
 
@@ -15,7 +19,7 @@ class category {
 
 	_changeCategory() {
 		const tenKey = document.querySelector('.ten-key');
-		const selectedCategory = document.querySelector('#pcCategory');
+		const selectedCategory = this.selectedCategory;
 		selectedCategory.addEventListener('change', function() {
 			if (this.value === 'note') {
 				tenKey.style.display = 'block';
@@ -26,7 +30,45 @@ class category {
 	}
 
 	_changeSpec() {
-		const spec = document.querySelector('.pc-spec');
+		const category = this.selectedCategory;
+		const selectedSpec = document.querySelector('#spec');
+		const spec = document.querySelector('.spec-detail');
+		const cpu = this.cpu;
+		const memory = this.memory;
+		const storage = this.storage;
+		selectedSpec.addEventListener('change', function(){
+			spec.style.display = 'block';
+			if (this.value === 'normal') {
+				cpu.value = 'i5';
+				memory.value = 8;
+				storage.value = 128;
+			} else if (this.value === 'plus') {
+				cpu.value = 'i5';
+				memory.value = 16;
+				storage.value = 128;
+			} else if (this.value === 'high') {
+				cpu.value = 'i7';
+				memory.value = 16;
+				storage.value = 256;
+			}
+		});
+	}
 
+	_normalSpec() {
+		this.cpu.value = 'i5';
+		this.memory.value = 8;
+		this.storage.value = 128;
+	}
+
+	_normalPlusSpec() {
+		this.cpu.value = 'i5';
+		this.memory.value = 16;
+		this.storage.value = 128;
+	}
+
+	_highSpec() {
+		this.cpu.value = 'i7';
+		this.memory.value = 16;
+		this.storage.value = 256;
 	}
 }
