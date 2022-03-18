5fab79809e8186013b2c85958ae6fe59cc94cfd4
takumi3110 <takumi929s@prestigein.com>
2022-03-18 15:35:01 +0900

    refactor:modules作成

--- static/js/stock/stock.js
+++ static/js/stock/stock.js
@@ -20,50 +20,6 @@ function formatDate(date, format) {
     return format;
 }
 
-const getCookie = (name) => {
-    if (document.cookie && document.cookie !== '') {
-        for (const cookie of document.cookie.split(';')) {
-            const [key, value] = cookie.trim().split('=');
-            if (key === name) {
-                return decodeURIComponent(value);
-            }
-        }
-    }
-}
-const csrfToken = getCookie('csrftoken');
-
-function makePutInit(data) {
-    return {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken
-        },
-        body: JSON.stringify(data),
-    }
-}
-
-// function makePostInit(data) {
-//     return {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'X-CSRFToken': csrfToken
-//         },
-//         body: JSON.stringify(data),
-//     }
-// }
-
-// function GetInit() {
-//     return {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'X-CSRFToken': csrfToken
-//         },
-//     }
-// }
-
 function changePrice(checkEl, totalEl, count) {
     totalEl.innerHTML = '';
     const tax = 1.10
