e964933f341fd40d1fb9c0d6168b4e9713e7680e
takumi3110 <takumi929s@prestigein.com>
2022-03-18 16:10:03 +0900

    add:fields id

--- quote/serializers.py
+++ quote/serializers.py
@@ -6,6 +6,6 @@ from .models import QuoteItem
 class QuoteItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = QuoteItem
-        fields = ('number', 'item', 'quantity', 'registration_at', 'ordered_at', 'ordered', 'arrived_at', 'delivery_at',
-                  'delivered', 'worker')
+        fields = ('id', 'number', 'item', 'quantity', 'registration_at', 'ordered_at', 'ordered', 'arrived_at',
+                  'delivery_at', 'delivered', 'worker')
  
