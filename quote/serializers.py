e4d95be174ee2b410c98dc5c37760c524cea711e
takumi3110 <takumi929s@prestigein.com>
2022-03-29 16:08:09 +0900

    add:OrderItem, OrderInfo

--- quote/serializers.py
+++ quote/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
 
-from .models import QuoteItem
+from .models import QuoteItem, OrderItem, OrderInfo
 
 
 class QuoteItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = QuoteItem
         fields = ('id', 'number', 'item', 'quantity', 'registration_at', 'ordered', 'worker')
- 
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ('id', 'destination', 'quote_item', 'ordered', 'ordered_at', 'arrived', 'arrived_at', 'delivered',
+                  'delivered_at')
+
+
+class OrderInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderInfo
+        fields = ('id', 'status', 'number', 'ticket', 'cart', 'registration_at', 'updated_at', 'finished',
+                  'finished_at')
