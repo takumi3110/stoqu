d52960122b8876220d276ebdb21c817a71792c76
takumi3110 <takumi929s@prestigein.com>
2022-03-30 13:25:08 +0900

    add:チェックボックスを監視。変更をpost

--- static/js/quote/quote.js
+++ static/js/quote/quote.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 	// new category();
 })
 
@@ -21,7 +21,7 @@ class category {
 		const tenKey = document.querySelector('.ten-key');
 		const selectedCategory = this.selectedCategory;
 		tenKey.style.display = 'none';
-		selectedCategory.addEventListener('change', function() {
+		selectedCategory.addEventListener('change', function () {
 			if (this.value === 'note') {
 				tenKey.style.display = 'block';
 			} else {
@@ -37,7 +37,7 @@ class category {
 		const cpu = this.cpu;
 		const memory = this.memory;
 		const storage = this.storage;
-		selectedSpec.addEventListener('change', function(){
+		selectedSpec.addEventListener('change', function () {
 			spec.style.display = 'block';
 			if (this.value === 'normal') {
 				cpu.value = 'i5';
@@ -74,7 +74,7 @@ class clickAllow {
 		const main = this.main;
 		const text = open.innerHTML
 		const newText = '閉じる<i class="fa-solid fa-angle-down allow selected"></i>'
-		open.addEventListener('click', function() {
+		open.addEventListener('click', function () {
 			if (main.classList.contains('toggle')) {
 				this.innerHTML = newText;
 				allow.classList.add('selected');
@@ -90,74 +90,73 @@ class clickAllow {
 
 class checkBoxObserver {
 	constructor() {
-		this.orderedCheckBox = document.querySelectorAll('.ordered');
-		this.arrivedCheckBox = document.querySelectorAll('.arrived');
-		this.deliveredCheckBox = document.querySelectorAll('.delivered');
+		this.checkBox = document.querySelectorAll('.checkBox');
 		this._init();
 	}
 
 	_init() {
-		const orderedCheckBox = this.orderedCheckBox;
-		this._checked();
+		const checkBox = this.checkBox;
+		this._checked(checkBox);
 	}
 
 	_checked(els) {
 		els.forEach(el => {
-			if (el.checked) {
-
-			}
-		})
-	}
-
-	_post(el) {
-
-	}
-
-}
-
-function get_request(url) {
-	let getInit = GetInit();
-	return fetch(url, getInit)
-		.then(response => {
-			if (response.ok) {
-				return response.json();
-			} else {
-				throw Error();
-			}
-		})
-		.then(data => {
-			console.log(data)
-		})
-}
-
-function ordered_check(data, pk) {
-	const els = document.querySelectorAll('.ordered');
-	const url = 'http://127.0.0.1:8000/api/v1/quote/orderItem/' + pk + '/';
-	els.forEach(el => {
-		el.addEventListener('change', function() {
-			if (el.checked) {
-				data.ordered = true;
-				const init = makePutInit(data);
-				fetch(url, init)
-					.then(response => {
-						if (response.ok) {
-							return response.json();
-						} else {
-							throw Error();
-						}
-					})
-					.then(data => {
-						console.log(data.ordered);
-					})
-			}
+			el.addEventListener('change', function () {
+				if (el.checked) {
+					const value = el.value.split('-')
+					const quoteItem = value[0];
+					const orderItem = value[1];
+					const destination = value[2];
+					const url = 'http://127.0.0.1:8000/api/v1/quote/orderItem/' + orderItem + '/';
+					let label = ''
+					let data = {
+						destination: destination,
+						quote_item: quoteItem
+					}
+					if (el.name === 'ordered') {
+						label = document.querySelector('label.ordered' + orderItem);
+						data.ordered = true;
+					} else if (el.name === 'arrived') {
+						label = document.querySelector('label.arrived' + orderItem);
+						data.arrived = true;
+					} else if (el.name === 'delivered') {
+						label = document.querySelector('label.delivered' + orderItem);
+						data.delivered = true;
+					}
+					const init = makePutInit(data);
+					fetch(url, init)
+						.then(response => {
+							if (response.ok) {
+								changeLabel(el, label);
+								return response.json();
+							} else {
+								throw Error();
+							}
+						})
+						.then(data => {
+							console.log(data);
+						})
+				}
+			});
 		});
-	});
+	}
 }
 
+function changeLabel(el, label){
+	el.disabled = true;
+	el.checked = true;
+	const date = new Date();
+	const year = date.getFullYear();
+	const month = date.getMonth() + 1;
+	const day = date.getDate();
+	const hour = date.getHours();
+	const minutes = date.getMinutes();
+	label.innerHTML = year + '年' + month + '月' + day + '日' + hour + ':' + minutes;
+}
 
 
 function change_quantity(el, data, pk) {
-	el.addEventListener('change', function() {
+	el.addEventListener('change', function () {
 		const url = 'http://127.0.0.1:8000/api/v1/quote/quoteItem/' + pk + '/';
 		data.quantity = this.value;
 		const init = makePutInit(data);
@@ -175,25 +174,25 @@ function change_quantity(el, data, pk) {
 	});
 }
 
-function all_apply(pk) {
-	const allApply = document.querySelector('.all-apply');
-	const destinationInput = document.querySelectorAll('.destination');
-	const destinationId = 'destination' + pk;
-	allApply.addEventListener('change', function() {
-		if (allApply.checked) {
-			console.log('checked');
-			// for(let input of destinationInput) {
-			// 	input.id = destinationId;
-			// }
-		} else {
-			console.log('none');
-		}
-	});
-}
+// function all_apply(pk) {
+// 	const allApply = document.querySelector('.all-apply');
+// 	const destinationInput = document.querySelectorAll('.destination');
+// 	const destinationId = 'destination' + pk;
+// 	allApply.addEventListener('change', function () {
+// 		if (allApply.checked) {
+// 			console.log('checked');
+// 			// for(let input of destinationInput) {
+// 			// 	input.id = destinationId;
+// 			// }
+// 		} else {
+// 			console.log('none');
+// 		}
+// 	});
+// }
 
 
 function modal_form(el, url) {
-	$(el).each(function() {
+	$(el).each(function () {
 		$(this).modalForm({formURL: url});
 	});
 }
